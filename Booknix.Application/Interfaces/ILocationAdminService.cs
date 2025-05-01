0c63d5b83e86ffbc6829df933105541f00d17c9b
ismail036 <ismailparlak036@gmail.com>
2025-05-01 13:02:45 +0300

    Revert "update"
    This reverts commit f9b60568382cea45a9a7ff17412b1347107ec68b.

--- Booknix.Application/Interfaces/ILocationAdminService.cs
+++ Booknix.Application/Interfaces/ILocationAdminService.cs
@@ -1,6 +1,6 @@
 ﻿using Booknix.Application.DTOs;
 using Booknix.Domain.Entities;
-using Booknix.Application.ViewModels;
+
 
 namespace Booknix.Application.Interfaces
 {
@@ -12,21 +12,5 @@ namespace Booknix.Application.Interfaces
         Task<RequestResult> DeleteLocationAsync(Guid Id);
         Task<Location?> GetLocationByIdAsync(Guid Id);
         Task<RequestResult> UpdateLocationAsync(Guid id, string name, string address, string phoneNumber, Guid sectorId);
-        
-        // Services
-        Task<List<Service>> GetServicesByLocationAsync(Guid locationId);
-        Task<ServiceCreateViewModel> GetServiceCreateViewModelAsync(Guid locationId);
-        Task<Service?> GetServiceByIdAsync(Guid id);
-        Task<RequestResult> AddServiceAsync(ServiceCreateDto model);
-        Task<RequestResult> UpdateServiceAsync(ServiceUpdateDto model);
-        Task<RequestResult> DeleteServiceAsync(Guid id);
-        Task<RequestResult> RemoveWorkerFromServiceAsync(Guid serviceId, Guid workerId);
-
-        // Workers
-        Task<IEnumerable<Worker>> GetWorkersByLocationAsync(Guid locationId);
-        Task<RequestResult> AddWorkerAsync(WorkerAddDto dto);
-        Task<RequestResult> UpdateWorkerAsync(WorkerUpdateDto dto);
-        Task<RequestResult> DeleteWorkerAsync(Guid id);
-        Task<Worker?> GetWorkerByIdAsync(Guid id);
     }
 }
